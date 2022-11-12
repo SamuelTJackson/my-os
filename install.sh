827d21728d7e9d3167a4590f15f21e57c2feccfb
Samuel <14303082+SamuelTJackson@users.noreply.github.com>
2022-11-12 20:42:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -236,8 +236,6 @@ add_zsh_config() {
 	cat >> "/home/${user}/.zshrc" <<EOF
 export LANG=en_US.UTF-8
 
-export LC_ALL=en_US.UTF-8
-
 # Path to your oh-my-zsh installation.
 export ZSH="/home/${user}/.oh-my-zsh"
 
