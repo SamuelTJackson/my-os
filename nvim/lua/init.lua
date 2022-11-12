e36d00770e1f64f04182cb25290513203d96c1a9
Samuel <14303082+SamuelTJackson@users.noreply.github.com>
2022-11-12 19:20:34 +0000

    Update init.lua

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -2,9 +2,3 @@ require('plugins')
 require('settings')
 require('key-mappings')
 require('auto-commands')
-
-require('lsp/config')
-require('lsp/formatter')
-require('lsp/diagnostic')
-
-require('plugins/buffer_jump')
