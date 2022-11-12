1b4f538dee252c909d3b6caa4b1d9939e787f2d0
Samuel <14303082+SamuelTJackson@users.noreply.github.com>
2022-11-12 19:33:11 +0000

    Create nvim-web-devicons.lua

---
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,5 @@
+require'nvim-web-devicons'.setup {
+    -- your personnal icons can go here (to override)
+    -- DevIcon will be appended to `name`
+    override = {mod = {icon = "", color = "#519aba", name = "Mod"}}
+}
