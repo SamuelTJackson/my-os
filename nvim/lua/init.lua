44e7555e8d18f5d546f5b4897cb9434923ef4ef2
Samuel <14303082+SamuelTJackson@users.noreply.github.com>
2022-11-12 19:15:51 +0000

    Create init.lua

---
+++ nvim/lua/init.lua
@@ -0,0 +1,10 @@
+require('plugins')
+require('settings')
+require('key-mappings')
+require('auto-commands')
+
+require('lsp/config')
+require('lsp/formatter')
+require('lsp/diagnostic')
+
+require('plugins/buffer_jump')
